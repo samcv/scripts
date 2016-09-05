40f094a177f3bd3d031c5e1dc59ed4d49e29a55b
Samantha McVey <samantham@posteo.net>
2016-09-05 05:00:26 -0700

    clean up and refactor

--- bash-history-archive.pl
+++ bash-history-archive.pl
@@ -5,28 +5,40 @@
 # A program to save bash history files to files named after the typed date.
 use strict;
 use warnings;
-my $home = `echo \$HOME`;
+my $home = $ENV{HOME};
 chomp $home;
+my $day;
+my $year;
+my $month;
 my $tempfile = $home . '/bash_history_temp';
 my $archive_dir = $home . "/bash-history";
-
+`mkdir -p $archive_dir`;
 open(my $in, '<', "$home/.bash_history" ) or die("Could not open file: $!");
 open my $out, '>', "$tempfile" or die "Can't write new file: $!";
 #date '+%m/%d/%y %H:%M:%S' -d @147306792
-my $var = 0;
+my $is_cmd_line = 0;
 foreach my $line (<$in>) {
   chomp $line;
-  if ($var == 1) {
-    $var = 0;
-    print $out "$line\n";
+  if ($is_cmd_line == 1) {
+    $is_cmd_line = 0;
+    open my $dated_file, '>>', "$archive_dir/$year-$month-$day-bash_history.txt" or die "Could not open file: $!";
+    print $dated_file "$line\n";
+    close $dated_file;
   }
   elsif ($line =~ /^#[0-9]+/) {
     $line =~ s/^#//;
     my $cmd = "date '+%Y/%m/%d %H:%M:%S' -d @" . "$line";
     my $date = `$cmd`;
     chomp $date;
-    print $out "$date  ";
-    $var = 1;
+    #print "date: $date\n";
+    ($year, $month, $day) = split /\//, $date, 3;
+    $day =~ s/ .*//;
+    #print "year: $year month: $month day: $day\n";
+    open my $dated_file, '>>', "$archive_dir/$year-$month-$day-bash_history.txt" or die "Could not open file: $!";
+    print $dated_file "$date  ";
+    $is_cmd_line = 1;
+    close $dated_file;
+
   }
   else {
     print $out "$line\n";
@@ -34,16 +46,3 @@ foreach my $line (<$in>) {
 }
 close $in;
 close $out;
-`mkdir -p $archive_dir`;
-open my $in2, '<', "$tempfile" or die "Could not open file: $!";
-foreach my $line2 (<$in2>) {
-  chomp $line2;
-  if ($line2 =~ /^[0-9][0-9][0-9][0-9]/) {
-    my($year, $month, $day) = split /\//, $line2, 3;
-    $day =~ s/ .*//;
-    open my $dated_file, '>>', "$archive_dir/$year-$month-$day-bash_history.txt" or die "Could not open file: $!";
-    print $dated_file "$line2\n";
-    close $dated_file;
-  }
-}
-`rm $tempfile`
