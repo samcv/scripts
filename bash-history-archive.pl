56bcd29bec4de20ecdb1aa27a9a23d739c97b00a
Samantha McVey <samantham@posteo.net>
2016-09-05 05:15:25 -0700

    minor cleanup

--- bash-history-archive.pl
+++ bash-history-archive.pl
@@ -7,55 +7,58 @@ use strict;
 use warnings;
 my $home = $ENV{HOME};
 chomp $home;
-my($year, $month, $day);
-my $current_epoch = time();
+my ( $year, $month, $day );
+my $current_epoch      = time();
 my $last_archive_epoch = `cat ~/bash-history/last-date`;
-if ($last_archive_epoch eq "") {
-  $last_archive_epoch = 0;
+
+if ( $last_archive_epoch eq "" ) {
+	$last_archive_epoch = 0;
 }
-my $tempfile = $home . '/bash_history_temp';
+my $tempfile    = $home . '/bash_history_temp';
 my $archive_dir = $home . "/bash-history";
 `mkdir -p $archive_dir`;
-open(my $in, '<', "$home/.bash_history" ) or die("Could not open file: $!");
+open( my $in, '<', "$home/.bash_history" ) or die("Could not open file: $!");
 open my $out, '>', "$tempfile" or die "Can't write new file: $!";
-#date '+%m/%d/%y %H:%M:%S' -d @147306792
+
 my $is_cmd_line = 0;
-my $skip = 0;
+my $skip        = 0;
 
 foreach my $line (<$in>) {
-  chomp $line;
-  if ($is_cmd_line == 1) {
-    $is_cmd_line = 0;
-    open my $dated_file, '>>', "$archive_dir/$year-$month-$day-bash_history.txt" or die "Could not open file: $!";
-    print $dated_file "$line\n";
-    close $dated_file;
-  }
-  elsif ( $skip == 1 ) {
-    $skip = 0;
-    last;
-  }
-  elsif ($line =~ /^#[0-9]+/) {
-    $line =~ s/^#//;
-    if ($line < $last_archive_epoch) {
-      $skip = 1;
-      last;
-    }
-    my $cmd = "date '+%Y/%m/%d %H:%M:%S' -d @" . "$line";
-    my $date = `$cmd`;
-    chomp $date;
-    #print "date: $date\n";
-    ($year, $month, $day) = split /\//, $date, 3;
-    $day =~ s/ .*//;
-    #print "year: $year month: $month day: $day\n";
-    open my $dated_file, '>>', "$archive_dir/$year-$month-$day-bash_history.txt" or die "Could not open file: $!";
-    print $dated_file "$date  ";
-    $is_cmd_line = 1;
-    close $dated_file;
+	chomp $line;
+	if ( $is_cmd_line == 1 ) {
+		$is_cmd_line = 0;
+		open my $dated_file, '>>',
+			"$archive_dir/$year-$month-$day-bash_history.txt"
+			or die "Could not open file: $!";
+		print $dated_file "$line\n";
+		close $dated_file;
+	}
+	elsif ( $skip == 1 ) {
+		$skip = 0;
+		last;
+	}
+	elsif ( $line =~ /^#[0-9]+/ ) {
+		$line =~ s/^#//;
+		if ( $line < $last_archive_epoch ) {
+			$skip = 1;
+			last;
+		}
+		my $cmd  = "date '+%Y/%m/%d %H:%M:%S' -d @" . "$line";
+		my $date = `$cmd`;
+		chomp $date;
+		( $year, $month, $day ) = split /\//, $date, 3;
+		$day =~ s/ .*//;
+		open my $dated_file, '>>',
+			"$archive_dir/$year-$month-$day-bash_history.txt"
+			or die "Could not open file: $!";
+		print $dated_file "$date  ";
+		$is_cmd_line = 1;
+		close $dated_file;
 
-  }
-  else {
-    print $out "$line\n";
-  }
+	}
+	else {
+		print $out "$line\n";
+	}
 }
 close $in;
 close $out;
