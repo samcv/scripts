1920171f9d815c337904105f3900bd5b8cd09f60
Samantha McVey <samantham@posteo.net>
2016-10-31 23:23:27 -0700

    update cd history

--- cd_history.sh
+++ cd_history.sh
@@ -8,9 +8,8 @@ save_loc () {
 		BACK_DIR[$BACK_NUM]="$(pwd)"
 	fi
 }
-alias tell_me='echo ${BACK_DIR[ $(($BACK_NUM - 0)) ]}'
 alias cd='BACK_DIR[$BACK_NUM]="$(pwd)"; let "BACK_NUM++"; builtin cd'
-alias tell='echo "History $(( $BACK_NUM + 0 )) Future $((${#BACK_DIR[@]} - $BACK_NUM - 0)) Prev dir: ${BACK_DIR[ $(($BACK_NUM - 1 )) ]} Next dir: ${BACK_DIR[$(($BACK_NUM + 1))]}"'
+alias tell='if [ $BACK_NUM -ne 0 ]; then echo "History $(( $BACK_NUM + 0 )) Future $((${#BACK_DIR[@]} - $BACK_NUM - 0)) Prev dir: ${BACK_DIR[ $(($BACK_NUM - 1 )) ]} Next dir: ${BACK_DIR[$(($BACK_NUM + 1))]}"; fi;'
 alias back='if [ "$BACK_NUM" -ge "0" ]; then save_loc; let "BACK_NUM--"; builtin cd "${BACK_DIR[$BACK_NUM]}"; else echo "Can'\''t go back any further"; fi'
 alias next='if [ ! "${BACK_DIR[$(($BACK_NUM + 1))]}" == "" ]; then builtin cd "${BACK_DIR[ $(( $BACK_NUM + 1 )) ]}"; let "BACK_NUM++"; else echo "Can'\''t go forward any further"; fi'
 BACK_NUM=0
