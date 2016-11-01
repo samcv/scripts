e41bd129d96308852c4619297099fb732135b5af
Samantha McVey <samantham@posteo.net>
2016-10-31 23:20:09 -0700

    update cd history

--- cd_history.sh
+++ cd_history.sh
@@ -3,7 +3,14 @@
 # Will save the history of directories you `cd` into. Type back to go back to
 # the previously cd'd directory. Back again to go to the cd'd directory before
 # that etc. etc.
-BACK_NUM=0
+save_loc () {
+	if [ "${BACK_DIR[$(($BACK_NUM + 0))]}" == "" ]; then
+		BACK_DIR[$BACK_NUM]="$(pwd)"
+	fi
+}
+alias tell_me='echo ${BACK_DIR[ $(($BACK_NUM - 0)) ]}'
 alias cd='BACK_DIR[$BACK_NUM]="$(pwd)"; let "BACK_NUM++"; builtin cd'
-alias tell='echo "BACKNUM = $BACK_NUM ${BACK_DIR[$BACK_NUM]}"'
-alias back='if [ "$BACK_NUM" -gt "0" ]; then let "BACK_NUM--"; builtin cd "${BACK_DIR[$BACK_NUM]}"; else echo "Can'\''t go back any further"; fi'
+alias tell='echo "History $(( $BACK_NUM + 0 )) Future $((${#BACK_DIR[@]} - $BACK_NUM - 0)) Prev dir: ${BACK_DIR[ $(($BACK_NUM - 1 )) ]} Next dir: ${BACK_DIR[$(($BACK_NUM + 1))]}"'
+alias back='if [ "$BACK_NUM" -ge "0" ]; then save_loc; let "BACK_NUM--"; builtin cd "${BACK_DIR[$BACK_NUM]}"; else echo "Can'\''t go back any further"; fi'
+alias next='if [ ! "${BACK_DIR[$(($BACK_NUM + 1))]}" == "" ]; then builtin cd "${BACK_DIR[ $(( $BACK_NUM + 1 )) ]}"; let "BACK_NUM++"; else echo "Can'\''t go forward any further"; fi'
+BACK_NUM=0
